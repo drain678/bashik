d4446c2344bb9b84db691e47e17a8064872f9965
drain678 <ekaterina1.zorina@gmail.com>
2023-05-19 18:29:30 +0300

    test_emptydir

--- tests/test_emptydir.sh
+++ tests/test_emptydir.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+dirr="empty"
+
+mkdir dirr
 chmod +x ../dz.sh
-result=`../dz.sh empty_dir`
-expected="empty_dir is an empty directory
+result=`../dz.sh dirr`
+expected="dirr is an empty directory
 Количество строчек кода: 0
 Количество строк с комментариями: 0
 Количество пустых строчек: 0"
 if [[ $result == $expected ]]
 then
     echo "тест на то, что эта папка пуста пройден успешно"
+    rm -r dirr
     exit 0
 else
     echo "тест на то, что эта папка пуста провален"
     echo "ожидалось: $expected" 
     echo "получено: $result" 
+    rm -r dirr
     exit 1
 fi
